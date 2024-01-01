c458f4191f861bdb0d7d9f62903764a08c578582
sairakshithreddy <sairakshitb@gmail.com>
2024-01-01 13:12:56 +0530

    commit

---
+++ src/CONDITIONS_LOOPS/Lab050.java
@@ -0,0 +1,18 @@
+package src.CONDITIONS_LOOPS;
+
+public class Lab050 {
+
+    public static void main(String[] args) {
+
+        if(true){
+            System.out.println("1");
+            if(true){
+                System.out.println("2");
+                if(true){
+                    System.out.println("3");
+                }
+            }
+        }
+    }
+
+}
